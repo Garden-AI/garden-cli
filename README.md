93883fe517c0dd99f76c45d01d8c366d0ee28e5b
Ben Blaiszik <blaiszik@uchicago.edu>
2023-02-02 10:41:38 -0600

    Basic CLI scaffolding and initial poetry configs

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# garden-cli
+# Garden Command Line Interface (CLI)
 CLI for working with Gardens
-
 
 `poetry install`
 
 `poetry run python garden-cli/garden.py create` 
 
+## Usage
+`garden create <params>` - Create a new garden
+
+
+# Development
+## Build
+`poetry build`
+
+## Publish
+`poetry publish --build`
--- garden-cli/garden.py
+++ garden-cli/garden.py
@@ -1,39 +1,35 @@
 import typer
+from typing import List, Optional
+from garden_ai import Garden
 import json
 import datetime
 
 app = typer.Typer()
 
-def create_garden(name: str, date_created: str, description: str):
+def create_garden(name: str, authors: List[str], description: str):
     garden = {
-        "name": name,
-        "date_created": date_created,
+        "title": name,
+        "authors": authors,
         "description": description
     }
     with open(f"garden.json", "w") as f:
-        json.dump(garden, f)
+        json.dump(garden, f, indent=4)
     typer.echo(f"Garden {name} created successfully.")
 
 @app.command()
 def create(
     name: str = typer.Option(None),
-    date_created: str = typer.Option(None),
+    authors: list[str] = typer.Option(None),
     description: str = typer.Option(None),
 ):
     """Create a Garden entity."""
     if not name:
         name = typer.prompt("Enter garden name: ")
-    if not date_created:
-        date_created = typer.prompt("Enter date created (YYYY-MM-DD): ", type=str)
-    while True:
-        try:
-            date_created = datetime.datetime.strptime(date_created, '%Y-%m-%d').strftime('%Y-%m-%d')
-            break
-        except ValueError:
-            date_created = typer.prompt("Invalid date format. Enter date created (YYYY-MM-DD): ", type=str)
+    if not authors:
+        authors = typer.prompt("Enter authors: ", type=str)
     if not description:
         description = typer.prompt("Enter description: ")
-    create_garden(name, date_created, description)
+    create_garden(name, authors, description)
 
 @app.command()
 def other(test=True):
